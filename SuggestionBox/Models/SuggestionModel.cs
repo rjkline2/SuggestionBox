461bbe0637d37ae2e2bd16a674b8405c8529b340
RJ Kline <rj.kline@gmail.com>
2018-03-05 14:24:58 -0500

    first Commit

---
+++ SuggestionBox/Models/SuggestionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionBox.Models
+{
+    public class SuggestionModel
+    {
+        [Key]
+        public int SuggestionID { get; set; }
+
+        public string Topic { get; set; }
+        public string Suggestion { get; set; }
+
+    }
+} 
